16d6d9d9fcadb000c4a1a65d1fc00d1348965b67
DameonWebb <dameonlwebb@gmail.com>
2022-06-15 16:09:44 -0700

    first React commit

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-
+//
 export default class App extends React.Component {
-  // your Javascript goes here
+  // constructor method to initialize the state of the app
+  constructor(props) {
+    this.state = {
+      balance: '',
+      rate:'',
+    
+    }
+  }
+  //add a property for ech input field to the state object and set their initial values
+  //create a function that updates the state values when an input changes, using event binding
   render() {
-    return (
-      <div className='container'>
-        {/* your JSX goes here */}
+    return ( 
+     //input element for mortgage loan balance
+     //input element for APR
+     <div className='container'>
+        {<input type = 'number' name='balance'></input>} 
+        {<input type = 'number' name='rate' step = '0.01'></input>}
+        {<select name='term'><option value = '15'></option><option value = '30'></option></select>}
+        {<button name = 'submit'>Submit</button>}
+        {<div name = 'output' id= 'output'></div>}
       </div>
     );
   }
